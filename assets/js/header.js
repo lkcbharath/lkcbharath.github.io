f5e82f626da398b6350a52afce7928e366853011
Bharath <lkcbharath@gmail.com>
2019-02-07 19:02:07 +0530

    Fixed errors)

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     var url = window.location.pathname;
     url = url.replace("\/",'')
     url = url.replace(".html",'')
+    if(url=="")url="home"
     $("#" + url).addClass("selected")
 });
 
