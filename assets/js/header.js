c369ac988fbe3a8fc2f02c1a0a8d6ddd06fd95ad
Bharath <lkcbharath@gmail.com>
2019-02-08 21:44:13 +0530

    Fixed all responsive text issues

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,8 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
     var url = window.location.pathname;
     url = url.replace("\/",'')
     url = url.replace(".html",'')
     if(url=="")url="home"
-    jQuery("#" + url).addClass("selected")
+    $("#" + url).addClass("selected")
 });
 
