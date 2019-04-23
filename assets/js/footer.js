83c798fa0409586c7a282402848be19cdaedebac
Bharath <lkcbharath@gmail.com>
2019-04-23 19:53:24 +0530

    Redone Home page, added modal to blog

--- assets/js/footer.js
+++
@@ -1,8 +0,0 @@
-// jQuery(document).ready(function() {
-//     var contentHeight = jQuery(window).height();
-//     var footerHeight = jQuery('.footer').height();
-//     var footerTop = jQuery('.footer').position().top + footerHeight;
-//     if (footerTop < contentHeight) {
-//         jQuery('.footer').css('margin-top', 10+ (contentHeight - footerTop) + 'px');
-//       }
-//     });
